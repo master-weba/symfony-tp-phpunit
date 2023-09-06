e0e484783f80389d57a0700924fbee7697df6d27
JPechberty <Ninposhine313>
2023-09-06 13:55:12 +0200

    Refactoring LogReaderService.php and LogsController.php

--- src/Controller/LogsController.php
+++ src/Controller/LogsController.php
@@ -2,8 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\LogTrace;
 use App\Service\LogReaderService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Filesystem\Exception\FileNotFoundException;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -12,12 +14,21 @@ class LogsController extends AbstractController
     #[Route('/logs', name: 'app_logs')]
     public function index(): Response
     {
-        $service = new LogReaderService();
-
-        $data = $service->parseFile();
+        try{
+            $service = new LogReaderService();
+            $data = $service->getEntries($_ENV['LOGS_FILE_PATH'].$_ENV["LOGS_FILE_NAME"]);
+            $data = array_map(function($entry){
+                return new LogTrace(date_create_immutable($entry["date"]),$entry["type"],$entry["message"]);
+            },$data);
+            $parseError = false;
+        }catch(FileNotFoundException $e){
+            $data = [];
+            $parseError = true;
+        }
 
         return $this->render('logs/index.html.twig', [
             'logs' => $data,
+            'parseError' => $parseError
         ]);
     }
 }
--- src/Service/LogReaderService.php
+++ src/Service/LogReaderService.php
@@ -2,10 +2,19 @@
 
 namespace App\Service;
 
+use Symfony\Component\Filesystem\Exception\FileNotFoundException;
+
 class LogReaderService
 {
-    public function parseFile(){
-        $json = file_get_contents("../".$_ENV['LOGS_FILE_PATH'].$_ENV["LOGS_FILE_NAME"]);
+    public function getEntries(string $filename){
+        $path = "../$filename";
+
+        if(file_exists($path) === false){
+            throw new FileNotFoundException();
+        }
+        $json = file_get_contents($path);
         return json_decode($json,true);
     }
+
+
 }
