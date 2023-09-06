e68c59b6a5a50687e2c145e5c8859e56289c3f4e
JPechberty <Ninposhine313>
2023-09-06 23:48:25 +0200

    Fix clean LogReaderService

--- src/Service/LogReaderService.php
+++ src/Service/LogReaderService.php
@@ -6,19 +6,10 @@ use Symfony\Component\Filesystem\Exception\FileNotFoundException;
 
 class LogReaderService
 {
-    /**
-     * @param string $filename
-     * @return mixed
-     */
-    public function getEntries(string $filename): mixed
+    public function __construct(
+        private readonly string $rootDir
+    )
     {
-        $path = "../$filename";
-
-        if(file_exists($path) === false){
-            throw new FileNotFoundException();
-        }
-        $json = file_get_contents($path);
-        return json_decode($json,true);
     }
 
 
